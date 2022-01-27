4162959aa50fa4e83e6cd5e4be614483d0b24fd7
谢达文 <xiedawen@ydjdev.com>
2022-01-27 11:05:01 +0800

    test

---
+++ views/home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Mytitle from './title';
+const index = () => {
+    return (
+        <div>
+            <h1>home 111</h1>
+            <Mytitle></Mytitle>
+        </div>
+    );
+};
+
+export default index;
